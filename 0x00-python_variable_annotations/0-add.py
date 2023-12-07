ad02f27a1493540b5a9d69228979407b59d11ab1
Castro Hamiltone <castromiltone@gmail.com>
2023-12-07 20:09:53 +0300

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+module declares a a type-annotated function add that takes a
+float a and a float b as arguments and returns their sum as a float
+"""
+
+def add(a: int, b: int) -> int:
+    """adds two integers"""
+    return a + b
+
