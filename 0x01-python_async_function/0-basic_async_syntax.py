d5f450bd4ea4ec50e008eb1222819a5a1cb741de
Castro Hamiltone <castromiltone@gmail.com>
2023-12-11 22:24:49 +0300

    fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,14 +5,15 @@ integer argument (max_delay, with a default value of 10)
 named wait_random that waits for a random delay between 0
 and max_delay (included and float value) seconds and eventually returns it.
 """
-from random import randint
-from time import sleep
+from random import random
+from asyncio import sleep
 
 
 async def wait_random(max_delay: int=10) -> float:
     """
     async coroutine that accepts an integer
     """
-    sleep(randint(0, max_delay))
+    wait = random() * max_delay
+    await sleep(wait)
 
-    return max_delay
+    return wait
