cfbcc97e0aa38cbd45b98b964174f3b21777402f
Castro Hamiltone <castromiltone@gmail.com>
2024-01-07 18:51:21 +0300

    TestIntegrationGithubOrgClient

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -149,3 +149,58 @@ class TestGithubOrgClient(TestCase):
         org_client = GithubOrgClient("google")
         client_has_licence = org_client.has_license(repo, key)
         self.assertEqual(client_has_licence, expected)
+
+
+class TestIntegrationGithubOrgClient(TestCase):
+    """
+    Create the TestIntegrationGithubOrgClient(unittest.TestCase) class and
+    implement the setUpClass and tearDownClass which are part of the
+    unittest.TestCase API.
+
+    Use @parameterized_class to decorate the class and parameterize it with
+    fixtures found in fixtures.py. The file contains the following fixtures:
+
+    org_payload, repos_payload, expected_repos, apache2_repos
+    The setupClass should mock requests.get to return example payloads found
+    in the fixtures.
+
+    Use patch to start a patcher named get_patcher, and use side_effect to make
+    sure the mock of requests.get(url).json() returns the correct fixtures for
+    the various values of url that you anticipate to receive.
+
+    Implement the tearDownClass class method to stop the patcher.
+    """
+    @classmethod
+    def setUpClass(cls) -> None:
+        """setup class before tests are ran"""
+        mock_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            if url in mock_payload:
+                return Mock(**{'json.return_value': mock_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """test case"""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """test case"""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """deletes class after tests are ran"""
+        cls.get_patcher.stop()
